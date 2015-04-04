6a95ca5aafeb7a5876e8b1cb7ee95c459ebb4dbf
SArehalli <sgarehal@ucsd.edu>
2015-04-04 13:06:38 -0700

    One dimensional hand tracking metronome

---
+++ main.py
@@ -0,0 +1,65 @@
+import pyglet 
+import mido
+import Leap, sys, thread, time
+from Leap import CircleGesture
+
+class ConductListener(Leap.Listener):
+    
+    def on_connect(self, controller):
+        print "-- Connected"
+
+        self.init_velocity = Leap.Vector(0,0,0)
+        self.init_time = 0
+        self.stopped = [False, False, False];
+        self.audio = pyglet.media.load('beep.wav', streaming=False) 
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
+
+    def on_disconnect(self, controller):
+        print "Disconnected"
+
+    def on_frame(self, controller):
+        # Get the current frame
+        frame = controller.frame()
+        hand = frame.hands[0]
+        
+        # get current position
+        curr_velocity = hand.palm_velocity.to_tuple()
+        curr_velocity = tuple(map(lambda x : 0 if abs(x) < 45 else x, 
+                                  curr_velocity)) 
+        # get a list of the coords that stayed the same (relatively)
+        result = [(curr_velocity[i] * self.init_velocity[i] <= 0)
+                   for i in range(3)]
+        
+        
+        # Check for change in the x dimension
+        toTick = False
+        for i in range(1):
+            if result[i] == True and not self.stopped[i]:
+                toTick = True
+                self.stopped[i] = True
+
+            elif result[i] == False and self.stopped[i]:
+                self.stopped[i] = False
+       
+        if toTick:
+            print "Tick"
+            self.audio = pyglet.media.load('beep.wav', streaming=False) 
+            self.audio.play()
+
+        # Update init velocity
+        self.init_velocity = curr_velocity
+
+if __name__ == "__main__":
+    # Create listener and controller
+    listener = ConductListener()
+    controller = Leap.Controller()
+
+    controller.add_listener(listener)
+
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+            controller.remove_listener(listener)
+
